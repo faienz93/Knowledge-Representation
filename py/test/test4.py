af475c4deab7b6f8ea989a93d7a4087e181819d5
antoniofaienza93 <afaienza93@gmail.com>
2018-09-17 11:43:45 +0200

    ADD test4 and test5 for insert and query

---
+++ py/test/test4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# from support import load_graph
+from SPARQLWrapper import SPARQLWrapper, JSON
+import rdflib
+
+# Carico il file contenente le triple da caricare sul triplestore
+g = rdflib.Graph()
+g.load("prova.owl")
+
+# Il grafo del triplestore su cui voglio caricare le triple
+tps_graph = "luca_angelucci" 
+
+# Costruisco la query usando sia il grafo finale sia le triple da caricare
+query = """INSERT DATA {
+GRAPH <%s> { %s }
+}""" % (tps_graph, g.serialize(format="nt"))
+
+
+# NB: Usare 'DELETE' al posto di 'INSERT' per rimuovere
+# i dati dal triplestore
+
+# Preparo la richiesta di
+# update e faccio la query
+sparql = SPARQLWrapper("http://localhost:3030/ds/update")
+sparql.setQuery(query)
+sparql.setMethod('POST') 
+q = sparql.query()
---
+++ py/test/test5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding=utf-8
+
+
+# Import the class that i want use
+from SPARQLWrapper import SPARQLWrapper
+
+# Set the end-point and ask if it give me the result to JSON
+sparql = SPARQLWrapper("http://localhost:3030/ds/query",returnFormat="json")
+
+# Create a new Query
+sparql.setQuery("""
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX sioc: <http://rdfs.org/sioc/ns#>
+PREFIX oa: <http://www.w3.org/ns/oa#>
+
+    SELECT DISTINCT ?person ?username WHERE {
+        ?person rdf:type foaf:Person .
+        ?user rdf:type sioc:UserAccount ;
+        sioc:account_of ?person .
+    OPTIONAL { ?user sioc:name ?username }
+    ?annotation rdf:type oa:Annotation ;
+        oa:annotatedBy ?user }
+    ORDER BY ?username""")
+
+
+# Run the query and print the result
+print sparql.query().convert()
