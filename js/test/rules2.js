06881f18c84105c248c500a965df277c273da12c
antoniofaienza93 <afaienza93@gmail.com>
2018-09-25 18:19:01 +0200

    Start to create a new Rule

---
+++ js/test/rules2.js
@@ -0,0 +1,42 @@
+"use strict";
+var reactor = null;
+
+function assert() { return reactor.assert.apply(reactor,arguments); }
+function not() { return reactor.not.apply(reactor,arguments); }
+function exists() { return reactor.exists.apply(reactor,arguments); }
+function forAll() { return reactor.forAll.apply(reactor,arguments); }
+
+function Professor(name,surname){
+    this.name = name;
+    this.surname = surname;
+}
+
+
+function CreateRules(){
+    reactor.createRule("ProfExists",0,{p: Professor},
+        [function(p) {
+            return p.name =="Mauro" && p.surname=="Gaspari";
+        }],
+        function(p) {
+            console.log("BELLO FIGO: " + p.name);
+        });
+    
+}
+
+function CreateInitialDS(){
+    var p = new Professor("Mauro","Gaspari");
+    reactor.assert(p);
+    reactor.trace(3);
+    reactor.run(Infinity,true,function() { 
+         console.log(JSON.stringify(p)); 
+    });
+}
+
+
+// costruttore iniziale
+function InitRuleReactor(){
+    reactor = new RuleReactor();
+    reactor.trace(3);
+    CreateRules();
+    CreateInitialDS();
+}
