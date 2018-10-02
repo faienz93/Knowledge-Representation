ee4339946561ca08ee82abed34de5d296778cb59
antoniofaienza93 <afaienza93@gmail.com>
2018-10-02 17:12:37 +0200

    [ADD] File of constant

---
+++ js/const.js
@@ -0,0 +1,24 @@
+/*
+ * ===========================================================================
+ * File: Const.js 
+ * Author: Antonio Faienza
+ * Desc: This file contains global variable
+ * ===========================================================================
+ */
+
+// start lessons
+var START_LESSONS = 9;
+
+// end lessons
+var END_LESSONS = 18;
+
+// Global calendar
+var calendar;
+
+// Momentjs calendar
+moment.locale('en');
+var now = moment();
+
+// Events of calendar
+var events = [];
+
